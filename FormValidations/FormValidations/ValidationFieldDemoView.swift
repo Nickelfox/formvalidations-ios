2199ba1bbb91562ca30e6cf843f7024764a0a7a8
Vaibhav <vaibhav@nickelfox.com>
2017-03-30 18:10:38 +0530

    Initial Commit

---
+++ FormValidations/FormValidations/ValidationFieldDemoView.swift
@@ -0,0 +1,23 @@
+//
+//  ValidationFieldDemoView.swift
+//  FormValidations
+//
+//  Created by Nickelfox on 30/03/17.
+//  Copyright © 2017 Nickelfox. All rights reserved.
+//
+
+import UIKit
+
+class ValidationFieldDemoView: UIView {
+
+    @IBOutlet var validationField: UITextField!
+
+    @IBAction func handleValidateTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func handleResetTapped(_ sender: Any) {
+        
+    }
+
+}
